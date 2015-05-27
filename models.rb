f6713db27b5efc3fc4b0dc7683824525458d29c6
Patrik Ragnarsson <patrik@starkast.net>
2015-05-27 15:09:04 +0200

    Use two spaces per indentation level
    This is the Ruby convention and best practice. :)

--- models.rb
+++ models.rb
@@ -7,182 +7,191 @@ require './golfse.rb'
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite://#{File.expand_path(File.dirname(__FILE__))}/test.db")
 
 class User
-    include DataMapper::Resource
-    property :id,           Serial
-    property :golfId,       String 
-    property :password,     String
+  include DataMapper::Resource
+  property :id,           Serial
+  property :golfId,       String
+  property :password,     String
 
-    has n, :bookings
+  has n, :bookings
 
-    def scrape
-        golfse = GolfSE.new(self)
-        golfse.scrape
+  def scrape
+    golfse = GolfSE.new(self)
+    golfse.scrape
+  end
+
+  def scrapeRounds
+    golfse = GolfSE.new(self)
+    golfse.scrapeRounds
+  end
+
+  def getCalendar
+    cal = Icalendar::Calendar.new
+
+    cal.timezone do |t|
+        t.tzid = "Europe/Stockholm"
     end
 
-    def scrapeRounds
-        golfse = GolfSE.new(self)
-        golfse.scrapeRounds
+    bookings.each do |booking|
+      cal.add_event(booking.ical)
     end
 
-    def getCalendar
-        cal = Icalendar::Calendar.new
-        cal.timezone do |t|
-            t.tzid = "Europe/Stockholm"
+    cal.publish
+    cal
+  end
+
+  def getResults
+    Course.all().map do |course|
+      {
+        :courseName => course.name,
+        :holes => course.holes.map do |hole|
+          {
+            :number => hole.number,
+            :results => hole.holeResults.all(:round => {:user => self}).map do |res|
+              {
+                'score' => res.score,
+                'date' => res.round.date,
+              }
+            end
+          }
         end
-        bookings.each do |booking|
-            cal.add_event(booking.ical)
-        end
-        cal.publish
-        cal
+      }
     end
-
-    def getResults
-        Course.all().map do |course|
-            {
-                :courseName => course.name,
-                :holes => course.holes.map do |hole|
-                    {
-                        :number => hole.number,
-                        :results => hole.holeResults.all(:round => {:user => self}).map do |res|
-                            {
-                                'score' => res.score,
-                                'date' => res.round.date,
-                            }
-                        end
-                    }
-                end
-            }
-        end
-    end
-
+  end
 end
 
 class Course
-    include DataMapper::Resource
+  include DataMapper::Resource
 
-    property :id,           Serial
-    property :name,         String
+  property :id,           Serial
+  property :name,         String
 
-    has n, :rounds
-    has n, :holes
+  has n, :rounds
+  has n, :holes
 end
 
 class Hole
-    include DataMapper::Resource
+  include DataMapper::Resource
 
-    property :id,           Serial
-    property :number,       Integer
-    property :par,          Integer
-    property :index,        Integer
+  property :id,           Serial
+  property :number,       Integer
+  property :par,          Integer
+  property :index,        Integer
 
-    belongs_to :course
-    has n, :holeResults
+  belongs_to :course
+  has n, :holeResults
 end
 
 class Round
-    include DataMapper::Resource
+  include DataMapper::Resource
 
-    property :id,           Integer,    :key => true
-    property :date,         DateTime
+  property :id,           Integer,    :key => true
+  property :date,         DateTime
 
-    belongs_to :course
-    belongs_to :user
-    has n, :holeResults
+  belongs_to :course
+  belongs_to :user
+  has n, :holeResults
 
-    def self.create_from_scrape(user, json_data)
-        date = DateTime.strptime(json_data['Date'], "%Y%m%dT%H%M%S")
-        id = json_data['RefId']
-        courseName = json_data['CourseName']
-        course = Course.first_or_create(:name => courseName)
-        Round.first_or_create(:date => date, :id => id, 
-                              :course => course, :user => user)
-    end
+  def self.create_from_scrape(user, json_data)
+    date = DateTime.strptime(json_data['Date'], "%Y%m%dT%H%M%S")
+    id = json_data['RefId']
+    courseName = json_data['CourseName']
+    course = Course.first_or_create(:name => courseName)
+    Round.first_or_create(:date => date, :id => id,
+                          :course => course, :user => user)
+  end
 
-    def scrapeHoles
-        golfse = GolfSE.new(self.user)
-        golfse.scrapeHoles(self)
-    end
+  def scrapeHoles
+    golfse = GolfSE.new(self.user)
+    golfse.scrapeHoles(self)
+  end
 end
 
 class HoleResult
-    include DataMapper::Resource
+  include DataMapper::Resource
 
-    property :id,           Serial
-    property :score,        Integer
+  property :id,           Serial
+  property :score,        Integer
 
-    belongs_to :round
-    belongs_to :hole
+  belongs_to :round
+  belongs_to :hole
 end
 
 class Booking
-    include DataMapper::Resource
+  include DataMapper::Resource
 
-    property :id,           Serial
-    property :uniqueId,     String,     :key => true
-    property :date,         DateTime
-    property :type,         String
-    property :typeStr,      String
-    property :course,       String
+  property :id,           Serial
+  property :uniqueId,     String,     :key => true
+  property :date,         DateTime
+  property :type,         String
+  property :typeStr,      String
+  property :course,       String
 
-    belongs_to :user
-    
-    has n, :coPlayers
+  belongs_to :user
 
-    def self.create_from_scrape(user, dateStr, timeStr, type, typeStr, course, players, uniqueId)
-        date = Date.strptime(dateStr)
-        if !timeStr.empty?
-            dateTimeStr = "#{dateStr}T#{timeStr}"
-            date = DateTime.strptime(dateTimeStr, "%Y-%m-%dT%H:%M")
-        end
-        b = Booking.first_or_create(:uniqueId => uniqueId, :user => user)
-        b.attributes = {:user => user, :date => date, :type => type, :typeStr => typeStr, :course => course}
-        players.each do |player|
-        
-            p = CoPlayer.first_or_create(:booking => b, :name => player[:name])
-            puts p
-            p[:hcp] = player[:hcp]
-            p.save
-        end
-        b.save
-        puts b.coPlayers
-        return b
+  has n, :coPlayers
+
+  def self.create_from_scrape(user, dateStr, timeStr, type, typeStr, course, players, uniqueId)
+    date = Date.strptime(dateStr)
+
+    if !timeStr.empty?
+      dateTimeStr = "#{dateStr}T#{timeStr}"
+      date = DateTime.strptime(dateTimeStr, "%Y-%m-%dT%H:%M")
     end
 
-    def to_s
-        return "#{@typeStr} at #{@course}, #{@date} <#{@uniqueId}>"
+    b = Booking.first_or_create(:uniqueId => uniqueId, :user => user)
+    b.attributes = {:user => user, :date => date, :type => type, :typeStr => typeStr, :course => course}
+    players.each do |player|
+      p = CoPlayer.first_or_create(:booking => b, :name => player[:name])
+      puts p
+      p[:hcp] = player[:hcp]
+      p.save
     end
 
-    def ical
-        event = Icalendar::Event.new
-        if @date.hour == 0
-            # Only date if we have no hours
-            event.dtstart = Icalendar::Values::Date.new(@date)
-        else
-            event.dtstart = @date
-            # End 4 hours later
-            event.dtend = @date + Rational(4, 24)
-        end
-        event.summary = @typeStr
-        event.location = @course
-        event.description = @typeStr
-        puts @CoPlayers
-        if @coPlayers
-            player_arr = @CoPlayer.map do |player|
-                "#{player.name} (#{player.hcp})"
-            end
-            event.description += " med " + player_arr.join(' och ')
-        end
-        return event
+    b.save
+    puts b.coPlayers
+
+    return b
+  end
+
+  def to_s
+    return "#{@typeStr} at #{@course}, #{@date} <#{@uniqueId}>"
+  end
+
+  def ical
+    event = Icalendar::Event.new
+
+    if @date.hour == 0
+      # Only date if we have no hours
+      event.dtstart = Icalendar::Values::Date.new(@date)
+    else
+      event.dtstart = @date
+      # End 4 hours later
+      event.dtend = @date + Rational(4, 24)
     end
+
+    event.summary = @typeStr
+    event.location = @course
+    event.description = @typeStr
+    puts @CoPlayers
+
+    if @coPlayers
+      player_arr = @CoPlayer.map do |player|
+          "#{player.name} (#{player.hcp})"
+      end
+      event.description += " med " + player_arr.join(' och ')
+    end
+
+    return event
+  end
 end
 
 class CoPlayer
-    include DataMapper::Resource
-    property :id,       Serial
-    property :name,     String
-    property :hcp,      String
+  include DataMapper::Resource
+  property :id,       Serial
+  property :name,     String
+  property :hcp,      String
 
-    belongs_to :booking
+  belongs_to :booking
 end
 
 DataMapper.finalize
